a1a3b247be922f9e7624898ce82f5ba1f0b96a05
dyushakolesov <dyushakolesov@mail.ru>
2020-05-30 20:20:58 +0300

    cloud model loading(instantiated), mp3 and sprite

---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+using System.IO;
+public class LoadAssetBundle : MonoBehaviour
+{
+    private string bundleURL = "ftp://89.208.87.29/spriteandmusic.unity3d";//??
+    private int version = 0;
+    [SerializeField] AudioSource source;
+    [SerializeField] SpriteRenderer sRenderer;
+    public void OnClickDownload()
+    {
+        StartCoroutine(DownloadAndCache());
+    }
+
+    IEnumerator DownloadAndCache()
+    {
+        while (!Caching.ready)
+            yield return null;
+
+        var www = WWW.LoadFromCacheOrDownload(bundleURL, version);
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        Debug.Log("Загрузка успешна");
+        var assetBundle = www.assetBundle;
+        string musicName = "cat_1_minute_sound.mp3";
+        string modelName = "berry.png";
+
+        var musicRequest = assetBundle.LoadAssetAsync(musicName, typeof(AudioClip));
+        yield return musicRequest;
+        Debug.Log("музыкальный файл распакован");
+
+        var modelRequest = assetBundle.LoadAssetAsync(modelName, typeof(Sprite));
+        yield return modelRequest;
+        Debug.Log("модель распакована");
+
+        source.clip = musicRequest.asset as AudioClip;
+        source.Play();
+        sRenderer.sprite = modelRequest.asset as Sprite;
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest uwr = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
+                var loadAsset = bundle.LoadAssetAsync<GameObject>("Assets/Players/MainPlayer.prefab");
+                yield return loadAsset;
+
+                Instantiate(loadAsset.asset);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LoadAssetBundleAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class LoadAssetBundleAnimation : MonoBehaviour 
+{
+    public string BundleURL = "ftp://89.208.87.29/model.unity3d";
+    public string AssetName;
+    public int version = 0;
+
+    void Start()
+    {
+        StartCoroutine(DownloadAndCache());
+    }
+
+    IEnumerator DownloadAndCache()
+    {
+        // Wait for the Caching system to be ready
+        while (!Caching.ready)
+            yield return null;
+
+        // Load the AssetBundle file from Cache if it exists with the same version or download and store it in the cache
+        using (WWW www = WWW.LoadFromCacheOrDownload(BundleURL, version))
+        {
+            yield return www;
+            if (www.error != null)
+                throw new Exception("WWW download had an error:" + www.error);
+            AssetBundle bundle = www.assetBundle;
+            if (AssetName == "")
+                throw new Exception("empty asset bundle name:" + www.error);
+            else
+            {
+                var prefab = bundle.LoadAsset<GameObject>("cat_Jump");
+                Instantiate(prefab);
+            }// Unload the AssetBundles compressed contents to conserve memory
+            bundle.Unload(false);
+
+        } // memory is freed from the web stream (www.Dispose() gets called implicitly)
+    }
+}
